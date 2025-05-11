5dbcb5ad1ef77559a51b66e749cdf49fc83b07fe
bassamxyz <beaxyzxyzxyz@gmail.com>
2025-05-11 13:16:03 +0300

    urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import index, orders, account
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", index.home, name="home"),
 ]
